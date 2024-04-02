38b7743f087ef3edd2e4975b0858fbf5db661a3c
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2024-04-02 22:21:59 +0300

    Fix indentation

--- src/SLIndent.sol
+++ src/SLIndent.sol
@@ -19,8 +19,8 @@ library slIndent {
     }
 
     function indentCount() pure internal returns (uint256) {
-        bytes memory payload = abi.encodeWithSignature("envOr(string,uint256)", InsetCountKey, 0);
-        return _sendReadU256Payload(payload);
+      bytes memory payload = abi.encodeWithSignature("envOr(string,uint256)", InsetCountKey, 0);
+      return _sendReadU256Payload(payload);
     }
 
     function indent() internal pure returns (uint256) {
@@ -44,51 +44,51 @@ library slIndent {
     function _castReadU256ToPure(
         function(bytes memory) internal view returns (uint256) fnIn
     ) internal pure returns (function(bytes memory) internal pure returns (uint256) fnOut) {
-        assembly {
-            fnOut := fnIn
-        }
+      assembly {
+          fnOut := fnIn
+      }
     }
 
     function _sendReadU256Payload(bytes memory payload) internal pure returns (uint256) {
-        return _castReadU256ToPure(_sendPayloadReadU256)(payload);
+      return _castReadU256ToPure(_sendPayloadReadU256)(payload);
     }
 
     function _sendPayloadReadU256(bytes memory payload) private view returns (uint256 returnValue) {
-        uint256 payloadLength = payload.length;
-        address vmAddress = slInternal.VM_ADDRESS;
-        assembly {
-          let payloadStart := add(payload, 32)
-          let output := mload(0x40) // Get a free memory pointer
-          let success := staticcall(gas(), vmAddress, payloadStart, payloadLength, output, 32)
-          if iszero(success) {
-            revert(0, 0)
-          }
-
-          returnValue := mload(output) // Load the returned uint256 value from memory to stack
-          mstore(0x40, add(output, 32)) // Update the free memory pointer
+      uint256 payloadLength = payload.length;
+      address vmAddress = slInternal.VM_ADDRESS;
+      assembly {
+        let payloadStart := add(payload, 32)
+        let output := mload(0x40) // Get a free memory pointer
+        let success := staticcall(gas(), vmAddress, payloadStart, payloadLength, output, 32)
+        if iszero(success) {
+          revert(0, 0)
         }
 
-        return returnValue;
+        returnValue := mload(output) // Load the returned uint256 value from memory to stack
+        mstore(0x40, add(output, 32)) // Update the free memory pointer
+      }
+
+      return returnValue;
     }
 
     function _castSetEnv(
-        function(bytes memory) internal view fnIn
+      function(bytes memory) internal view fnIn
     ) internal pure returns (function(bytes memory) internal pure fnOut) {
-        assembly {
-            fnOut := fnIn
-        }
+      assembly {
+          fnOut := fnIn
+      }
     }
 
     function _sendSetEnvPayload(bytes memory payload) internal pure  {
-        return _castSetEnv(_sendPayloadSetEnv)(payload);
+      return _castSetEnv(_sendPayloadSetEnv)(payload);
     }
 
     function _sendPayloadSetEnv(bytes memory payload) private view {
-        uint256 payloadLength = payload.length;
-        address vmAddress = slInternal.VM_ADDRESS;
-        assembly {
-            let payloadStart := add(payload, 32)
-            let success := staticcall(gas(), vmAddress, payloadStart, payloadLength, 0, 0)
-        }
+      uint256 payloadLength = payload.length;
+      address vmAddress = slInternal.VM_ADDRESS;
+      assembly {
+        let payloadStart := add(payload, 32)
+        let success := staticcall(gas(), vmAddress, payloadStart, payloadLength, 0, 0)
+      }
     }
 }
